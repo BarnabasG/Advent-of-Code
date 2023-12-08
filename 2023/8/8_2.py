5d9d2b4c7f812ab307d083f2000e1cf6bf92c59e
BarnabasG <BarnabasG@users.noreply.github.com>
2023-12-08 10:40:42 +0000

    update day 8 part 2

--- 2023/8/8_2.py
+++ 2023/8/8_2.py
@@ -6,11 +6,8 @@ def parse_input(filename):
     with open(filename, 'r') as f:
         directions = f.readline().rstrip('\n').replace('L', '0').replace('R', '1')
         f.readline()
-        instructions = {x[0]: (x[1],x[2]) for x in [parse_instructions(line.rstrip('\n')) for line in f.readlines()]}
+        instructions = {x[0]: (x[1],x[2]) for x in [re.findall(r'\w+', line.rstrip('\n')) for line in f.readlines()]}
         return directions, instructions
-    
-def parse_instructions(instruction):
-    return re.findall(r'\w+', instruction)
 
 def get_steps_to_z(m, node, directions):
     steps = 0
@@ -18,7 +15,6 @@ def get_steps_to_z(m, node, directions):
         node = m[node][int(directions[steps%len(directions)])]
         steps += 1
     return steps
-
 
 def get_answer(filename='input.txt'):
     directions, m = parse_input('2023/8/'+filename)
